21ec8637c2e388af400437a2d55e226c1feb6fe6
Артём Миков <artem.mikov.2003@mail.ru>
2023-11-06 22:05:34 +0500

    Написание фильтра для статистики

---
+++ DataLayer/StatisticFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Enum;
+namespace DataLayer
+{
+    public class StatisticFilter
+    {
+        [Range(1900,3000)]
+        public int Year { get; set; }
+        [Range(1,12)]
+        public int Month { get; set; }
+        public TypeTransaction TypeTransaction { get; set; }
+        public TypeGroup TypeGroup { get; set; }
+        public long AccountId { get; set; }
+        public bool AllAccounts { get; set; }
+        public bool AllTime { get; set; }
+
+    }
+}
