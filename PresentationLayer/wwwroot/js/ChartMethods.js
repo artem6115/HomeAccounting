c923a1bc992014d6ed9b5c1ded2bfaf62937bae2
Артём Миков <artem.mikov.2003@mail.ru>
2023-11-15 21:24:25 +0500

    Реализация графика категорий
    -Корректная очистка
    -График с цветовой политрой из библеотеки
    -Не работает выезд частей при наведнее , из за старой версии

---
+++ PresentationLayer/wwwroot/js/ChartMethods.js
@@ -0,0 +1,43 @@
+﻿
+function ParseData(json) {
+    let labels = [];
+    let points = [];
+    for (let i = 0; i < json.length; i++) {
+        labels[i] = json[i].description;
+        points[i] = json[i].x;
+        console.log(json[i]);
+
+
+    }
+    console.log(labels);
+    console.log(points);
+
+    let data = {
+        labels: labels,
+        datasets: [{
+            label: 'Сумма',
+            data :points,
+        }],
+    };
+    return data;
+}
+
+//function ParseData(json) {
+//    let labels = [];
+//    let data = [];
+//    for (let i = 0; i < json.length; i++) {
+//        labels[i] = json[i].Description;
+//        data[i] = json[i].X;
+
+//    }
+//    let data = {
+//        labels: labels,
+//        datasets: [{
+//            label: 'Сумма',
+//            data: data,
+
+//            hoverOffset: 4
+//        }]
+//    };
+//    return data;
+//}
